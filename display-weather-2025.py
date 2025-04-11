3ad4365718e5f40d2cabb46ce754d79cf617a7ee
Trotter73 <martin_e_coleman@hotmail.com>
2025-04-11 16:13:38 +0100

    Update display-weather-2025.py
    Lots of changes, some that I can remember;
    a) Reworked the weather display, now shows an icon for current and forecast conditions.
    
    b) The moon phase routine was totally broken, removed the ephem routine and replaced it with some code by Sean B. Palmer, http://inamidst.com/code/moonphase.py, all credits to him thanks.
    
    c) Now shows an png of the current moon phase.
    
    d) Tidied up the text presentation.
    
    e) Probably other stuff that I cant remember.

--- display-weather-2025.py
+++ display-weather-2025.py
@@ -4,6 +4,8 @@ import requests as rq
 import time
 import sys
 import unicornhathd
+import decimal
+import math
 from io import BytesIO
 from datetime import datetime, timedelta
 
@@ -18,11 +20,6 @@ except ImportError:
     sys.exit('This script requires the portolan module\nInstall with: sudo pip install portolan')
 
 try:
-    import ephem
-except ImportError:
-    sys.exit('This script requires the ephem module\nInstall with: sudo pip install ephem')
-
-try:
     from PIL import Image, ImageDraw, ImageFont, ImageOps
 except ImportError:
     sys.exit('This script requires the pillow module\nInstall with: sudo pip install pillow')
@@ -32,7 +29,9 @@ try:
 except ImportError:
     sys.exit('This script requires the feedparser module\nInstall with: sudo pip install feedparser')
 
+
 api_key = "YOUR OPENWEATHERMAP API KY HERE"
+
 disp_units = "metric"
 
 # Enter your lattitue & longitude here, for weather and moon phase functions.
@@ -42,17 +41,16 @@ longitude = 0
 # You should be able to change this to any RSS feed, but it will only pick the latest entry in that feed.
 news_feed = "http://feeds.bbci.co.uk/news/rss.xml?edition=uk"
 
-# What to display between data
 spacer = "* * * * * *"
+
+dec = decimal.Decimal
 
 # Choose a font.
 #
 # Use sudo apt install fontconfig & `fc-list` to show a list of installed fonts on your system,
 # or `ls /usr/share/fonts/` and explore.
-#
 FONT = ('/usr/share/fonts/truetype/roboto/unhinted/RobotoTTF/Roboto-Medium.ttf', 10)
-#
-#FONT = ('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 10)
+##FONT = ('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 10)
 #
 # sudo apt install fonts-freefont-ttf
 # FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 12)
@@ -87,7 +85,7 @@ def get_weather(api_key, lat, lon, units=disp_units):
     if len(today) == 0:
         today = list(filter(lambda l: same_day(l['dt'], tomorrow=True), forecast['list']))
 
-    high = -1000 
+    high = -1000
     wind = 0
     wind_direction = 0
     description = ""
@@ -132,29 +130,57 @@ def get_latest_news():
         latest_news = "No news available."
     return latest_news
 
-def get_moon_phase():
-    now = datetime.now()
-    moon = ephem.Moon()
-    moon.compute(now)
-    phase = moon.phase
-    if 0 <= phase < 1.84:
-        return "New Moon"
-    elif 1.84 <= phase < 5.53:
-        return "Waxing Crescent"
-    elif 5.53 <= phase < 9.22:
-        return "First Quarter"
-    elif 9.22 <= phase < 12.91:
-        return "Waxing Gibbous"
-    elif 12.91 <= phase < 16.61:
-        return "Full Moon"
-    elif 16.61 <= phase < 20.30:
-        return "Waning Gibbous"
-    elif 20.30 <= phase < 23.99:
-        return "Last Quarter"
-    elif 23.99 <= phase < 27.68:
-        return "Waning Crescent"
-    else:
-        return "New Moon"
+# The code for this function was taken from here, http://inamidst.com/code/moonphase.py , 
+# All credits to Sean B. Palmer, thanks.
+def get_moon_phase(now=None):
+    """Returns the current moon phase as a string."""
+    if now is None:
+        now = datetime.now()
+
+    diff = now - datetime(2001, 1, 1)
+    days = dec(diff.days) + (dec(diff.seconds) / dec(86400))
+    lunations = dec("0.20439731") + (days * dec("0.03386319269"))
+    pos = lunations % dec(1)
+
+    index = (pos * dec(8)) + dec("0.5")
+    index = math.floor(index)
+
+    phases = {
+        0: "New Moon",
+        1: "Waxing Crescent",
+        2: "First Quarter",
+        3: "Waxing Gibbous",
+        4: "Full Moon",
+        5: "Waning Gibbous",
+        6: "Last Quarter",
+        7: "Waning Crescent"
+    }
+
+    #return phases[int(index) & 7], round(float(pos), 3)
+    return phases[int(index) & 7]
+
+
+def display_moon_image():
+    """Fetches moon phase, selects corresponding icon, and opens the image."""
+    moon_icons = {
+        "New Moon": "icons/new_moon.png",
+        "Waxing Crescent": "icons/waxing_crescent.png",
+        "First Quarter": "icons/first_quarter.png",
+        "Waxing Gibbous": "icons/waxing_gibbous.png",
+        "Full Moon": "icons/full_moon.png",
+        "Waning Gibbous": "icons/waning_gibbous.png",
+        "Last Quarter": "icons/last_quarter.png",
+        "Waning Crescent": "icons/waning_crescent.png"
+    }
+
+    try:
+        moon_phase = get_moon_phase()  # Retrieve the moon phase
+        icon_path = moon_icons.get(moon_phase, "icons/default.png")  # Get the corresponding icon
+        return Image.open(icon_path)  # Open and return the image
+    except Exception as e:
+        print(f"Error loading moon image: {e}")
+        return None  # Return None if an error occurs
+
 
 def get_icon(weather_id):
     if weather_id in range(200, 233):
@@ -179,7 +205,7 @@ def get_icon(weather_id):
         icon = '03d'
     elif weather_id in range(803, 805):
         icon = '04d'
-        
+
     url = f"http://openweathermap.org/img/wn/{icon}@2x.png"
     resp = rq.get(url)
     if resp:
@@ -204,13 +230,34 @@ def display_icon(img):
     if valid:
         unicornhathd.show()
 
-def build_text(current, forecast):
-    text = " Temp: {:0.0f} Feels Like: {:0.0f} Wind: {:0.1f} {} Humidity: {}% ".format(
-        current['main']['temp'], current['main']['feels_like'], current['wind']['speed'], current['wind_direction'], current['main']['humidity']
-    )
-    text += " Forecast: {} High: {:0.0f} Wind: {:0.1f} {} Humidity: {}%".format(
+def display_micon(img):
+    unicornhathd.rotation(180)
+    unicornhathd.brightness(0.5)
+    width, height = unicornhathd.get_shape()
+    # Resize with high-quality resampling
+    img = img.resize((width, height), Image.Resampling.LANCZOS)
+    # Enhance contrast slightly for better visibility
+    img = ImageOps.autocontrast(img)
+    valid = False
+    for x in range(width):
+        for y in range(height):
+            pixel = img.getpixel((y, x))
+            r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
+            if r or g or b:
+                valid = True
+            unicornhathd.set_pixel(x, y, r, g, b)
+    if valid:
+        unicornhathd.show()
+
+def current_text(current):
+    c_text = "    {} Temp: {:0.0f} Feels Like: {:0.0f} Wind: {:0.1f} {} Humidity: {}% ".format(
+        current['weather'][0]['description'], current['main']['temp'], current['main']['feels_like'], current['wind']['speed'], current['wind_direction'], current['main']['humidity'])
+    return c_text
+
+def forecast_text(forecast):
+    fc_text = "    {} High: {:0.0f} Wind: {:0.1f} {} Humidity: {}%".format(
         forecast['description'], forecast['temp'], forecast['wind'], forecast['wind_direction'], forecast['humidity'])
-    return text
+    return fc_text
 
 def display_date_time():
     now = datetime.now().strftime(" %d %B %Y - %H:%M")
@@ -259,20 +306,36 @@ def main():
             current, forecast = get_weather(api_key, latitude, longitude)
             sunrise, sunset = get_sun_times(api_key, latitude, longitude)
             moon_phase = get_moon_phase()
-            text = build_text(current, forecast)
-            icon = get_icon(forecast['id'])
+            ctext = current_text(current)
+            ftext = forecast_text(forecast)
+            current_icon = get_icon(current['weather'][0]['id'])
+            forecast_icon = get_icon(forecast['id'])
             latest_news = get_latest_news()
-            t_end = time.time() + 60 * 6 # Update all the data every 6 minutes
+            moon_pic = display_moon_image()
+            t_end = time.time() + 60 * 6
             while time.time() < t_end:
                 dnt = display_date_time()
                 display_text(dnt)
-                display_icon(icon)
-                time.sleep(6)
-                display_text(text)
+                time.sleep(1)
+                display_text(spacer)
+                display_text("Current:-")
+                time.sleep(0.5)
+                display_icon(current_icon)
+                time.sleep(4)
+                display_text(ctext)
+                display_text(spacer)
+                display_text("Forecast:-")
+                time.sleep(0.5)
+                display_icon(forecast_icon)
+                time.sleep(4)
+                display_text(ftext)
                 display_text(spacer)
                 display_text(f" Sunrise: {sunrise} Sunset: {sunset} ")
                 display_text(spacer)
                 display_text(f" Moon Phase: {moon_phase} ")
+                time.sleep(0.5)
+                display_micon(moon_pic)
+                time.sleep(4)
                 display_text(spacer)
                 display_text(f" Latest News: {latest_news} ")
                 display_text(spacer)
